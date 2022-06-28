549dfc3f2ad1d44cdab1699eab9b41e670e0e658
Dani BL <danibachsl@protonmail.com>
2022-06-28 12:43:09 +0200

    lab acabado

--- routes/drones.js
+++ routes/drones.js
@@ -37,7 +37,7 @@ router.post('/drones/create', (req, res, next) => {
       maxSpeed: maxSpeed
     })
     .then(() => {
-      res.redirect("/drones")
+      res.redirect("/drones");
     })
     .catch(() => {
       res.redirect("/drones/create");
@@ -46,12 +46,14 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  Drone.findById(req.params.id)
+  const {id} = req.params
+
+  Drone.findById(id)
     .then((response) => {
-      res.render("drones/update-form.hbs", { response });
+      res.render("drones/update-form.hbs", response);
     })
-    .catch((err) => {
-      next(err);
+    .catch(() => {
+      res.redirect("/drones/create");
     });
 });
 
@@ -60,8 +62,9 @@ router.post('/drones/:id/edit', (req, res, next) => {
     const name = req.body.name;
     const propellers = req.body.propellers;
     const maxSpeed = req.body.maxSpeed;
+    const {id} = req.params
     Drone
-      .findByIdAndUpdate({ id: req.params._id })
+      .findByIdAndUpdate(id, {name, propellers, maxSpeed})
       .then(() => {
         res.redirect("/drones")
       })
@@ -72,8 +75,9 @@ router.post('/drones/:id/edit', (req, res, next) => {
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
+  const {id} = req.params
   Drone
-    .findByIdAndDelete({ id: req.params._id })
+    .findByIdAndDelete(id)
     .then(() => {
       res.redirect("/drones")
     })
